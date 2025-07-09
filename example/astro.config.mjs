e81705017fdfdd6a4c1dd3d5e1f81fc30bbf820b
okaryo <okaryotr@gmail.com>
2025-07-09 21:17:15 +0900

    chore(docs): update tailwindcss to v4 in example

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import remarkLinkCard from 'remark-link-card-plus';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   markdown: {
     remarkPlugins: [
       [
